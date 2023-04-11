ecdd3629601f15f2ab678b3afc4171be8011c916
IsaiahGz <isaiahgala99@gmail.com>
2023-04-11 19:47:35 -0400

    Loop starter prompt

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const { startPrompt } = require('./lib/prompt');
+const { startPrompt, starterOptions } = require('./lib/prompt');
 
-const init = () => {
-	startPrompt((answer) => {
-		console.log(answer);
-	});
+const init = async () => {
+	let shouldContinue = true;
+	const { choice } = await startPrompt();
+	switch (choice) {
+		case starterOptions.viewDepartment:
+			// View all departments
+			break;
+		case starterOptions.quit:
+			shouldContinue = false;
+			console.log('Goodbye!');
+			break;
+	}
+	if (shouldContinue) init();
 };
 
 init();
